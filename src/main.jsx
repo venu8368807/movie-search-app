6e00ddf315211de47329b913375e4233530bb8c0
Venu Bellamkonda <venubellomkonda6@gmail.com>
2025-09-28 01:43:15 +0530

    Fix main.jsx import path for case-sensitive build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import './Components/MovieSearchApp/CSS/index.css'
+import "./components/MovieSearchApp/CSS/index.css";
 
 
 createRoot(document.getElementById("venu")).render(
